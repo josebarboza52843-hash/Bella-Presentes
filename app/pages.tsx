e890a9eaf1fe95d4f32fb55e7a6f46cfb22d2f1f
josebarboza52843-hash <josebarboza52843@gmail.com>
2026-09-13 14:58:07 -0300

    Create pages.tsx

---
+++ app/pages.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <iframe
+        src="/index.html"
+        title="Bella Presentes Personalizados"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          display: "block",
+        }}
+      />
+    </main>
+  );
+}
